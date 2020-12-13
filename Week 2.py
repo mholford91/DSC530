67ec9da7f86a7ce1bb308479346ef0a8c60037d8
mholford91 <66338989+mholford91@users.noreply.github.com>
2020-12-13 17:46:24 -0500

    Add files via upload

---
+++ Week 2.py
@@ -0,0 +1,33 @@
+# Megan Holford
+# Week 2 Assignment
+# DSC 530 Winter 2020
+
+# The "Hello World!" string.
+print("Hello World! I wonder why that is always the default coding text to start with.")
+
+# Adding
+print("This is addition: 2+2= ", 2+2)
+
+# Subtracting
+print("This is subtraction: 4-2= ", 4-2)
+
+# Multiplying
+print("This is multiplication: 2x5= ", 2*5)
+
+# Dividing
+print("This is division: 10/2= ", int(10/2))
+
+# Concatentating two strings.
+print("Hello World! " + "I wonder why that is always the default coding text to start with.")
+
+# Making a list.
+my_list = ["Megan", 29, "July", "Florida"]
+print(my_list)
+
+# Checking it twice! I forgot to add something. Let's append and add it.
+my_list.append("Cat")
+print("Updated list: ", my_list)
+
+# Making a tuple.
+my_tuple = ("Miles", 11, "Margo", 7)
+print(my_tuple)
